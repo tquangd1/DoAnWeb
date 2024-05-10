fdcf022ae6e9613ce7f9ba2d5cd904fbd8316db0
tquangd1 <162397376+tquangd1@users.noreply.github.com>
2024-05-10 18:10:55 +0700

    Update lietke.php

--- Webbandocongnghe/admin/modules/quanlysp/lietke.php
+++ Webbandocongnghe/admin/modules/quanlysp/lietke.php
@@ -1,5 +1,5 @@
 <?php
-$sql_lietke_sp = "SELECT * FROM tbl_sanpham order by id_sanpham DESC";
+$sql_lietke_sp = "SELECT * FROM tbl_sanpham,tbl_danhmuc WHERE tbl_sanpham.id_danhmuc=tbl_danhmuc.id_danhmuc order by id_sanpham DESC";
 $query_lietke_sp = mysqli_query($mysqli, $sql_lietke_sp);
 ?>
 <p>Liệt kê sản phẩm</p>
@@ -10,6 +10,7 @@ $query_lietke_sp = mysqli_query($mysqli, $sql_lietke_sp);
         <th>Hình Ảnh</th>      
         <th>Giá Sản Phẩm</th>
         <th>Số Lượng</th>
+        <th>Danh Mục</th>
         <th>Mã Sản Phẩm</th>
         <th>Tóm Tắt</th>
         <th>Trạng Thái</th>
@@ -26,12 +27,19 @@ $query_lietke_sp = mysqli_query($mysqli, $sql_lietke_sp);
         <td><img src="modules/quanlysp/uploads/<?php echo $row['hinhanh'] ?>"width="150px"></td>
         <td><?php echo $row['giasp'] ?></td>
         <td><?php echo $row['soluong'] ?></td>
+        <td><?php echo $row['tendanhmuc'] ?></td>
         <td><?php echo $row['masp'] ?></td>
         <td><?php echo $row['tomtat'] ?></td>
-        <td><?php echo ($row['tinhtrang'] == 1) ? 'Kích Hoạt' : 'Ẩn'; ?></td>
+        <td><?php if($row['tinhtrang'] == 1){
+            echo 'Kích hoạt';
+        }else{
+            echo 'Ẩn';
+        }
+        ?>
+        </td>
         <td>
             <a href="modules/quanlysp/xuly.php?idsanpham=<?php echo $row['id_sanpham'] ?>">Xóa</a> |
-            <a href="?action=quanlysanpham&query=sua&idsanpham=<?php echo $row['id_sanpham'] ?>">Sửa</a>
+            <a href="?action=quanlysp&query=sua&idsanpham=<?php echo $row['id_sanpham'] ?>">Sửa</a>
         </td>
     </tr>
     <?php 
