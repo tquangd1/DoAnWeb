631d88d82c5f32d1ad208495c61947848ab04bc4
tquangd1 <162397376+tquangd1@users.noreply.github.com>
2024-05-10 18:17:19 +0700

    Update main.php

--- Webbandocongnghe/pages/main.php
+++ Webbandocongnghe/pages/main.php
@@ -19,11 +19,12 @@
                elseif($tam=='tintuc'){
                     include("main/tintuc.php");
                }
+
                elseif($tam=='lienhe'){
                     include("main/lienhe.php");
                }
-               elseif($tam=='dangky'){
-                    include("main/dangky.php");
+               elseif($tam=='sanpham'){
+                    include("main/sanpham.php");
                }
                else{
                     include("main/index.php");
