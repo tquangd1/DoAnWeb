e3912c117050e22a7449dbbd1b81919a35335638
tquangd1 <162397376+tquangd1@users.noreply.github.com>
2024-05-10 18:17:00 +0700

    Update sidebar.php

--- Webbandocongnghe/pages/sidebar/sidebar.php
+++ Webbandocongnghe/pages/sidebar/sidebar.php
@@ -1,10 +1,14 @@
 <div class="sidebar">
         <ul class="list_sidebar">
-             <li><a href="index.php?quanly=danhmucsanpham&id=1">Ốp lưng</a></li>
-            <li><a href="index.php?quanly=danhmucsanpham&id=2">Tai nghe</a></li>       
-            <li><a href="index.php?quanly=danhmucsanpham&id=3">Sạc điện thoại</a></li>
-            <li><a href="#">Dán màn hình </a></li>
-            <li><a href="#">Gậy tự sướng</a></li>
-            <li><a href="#">Lót ốp lưng</a></li>
-        </ul>              
+             
+             <?php
+                $sql_danhmuc = "SELECT * From tbl_danhmuc ORDER BY id_danhmuc DESC";
+                $query_danhmuc = mysqli_query($mysqli,$sql_danhmuc);
+                while($row =mysqli_fetch_array($query_danhmuc)){
+                                
+            ?>
+            <li><a href="index.php?quanly=danhmucsanpham&id=<?php echo $row['id_danhmuc']?>"><?php echo $row['tendanhmuc']?></a></li>
+            <?php
+            }
+            ?>
     </div>
