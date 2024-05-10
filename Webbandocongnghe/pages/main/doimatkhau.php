8170097d3c5310fcf7284cbe06bcc58084919ca5
tquangd1 <162397376+tquangd1@users.noreply.github.com>
2024-05-10 18:19:12 +0700

    Update doimatkhau.php

--- Webbandocongnghe/pages/main/doimatkhau.php
+++ Webbandocongnghe/pages/main/doimatkhau.php
@@ -1,1 +1,22 @@
-
+        <form action="" autocomplete="off" method="POST">
+            <table border="1" class="table-login" style="text-align:center; border-collapse:collapse;">
+                <tr>
+                    <td colspan="2"><h3>Đổi Mật Khẩu Admin</h3></td>
+                </tr>
+                <tr>
+                    <td>Tài Khoản</td>
+                    <td><input type="text" name="username"></td>
+                </tr>
+                <tr>
+                    <td>Mật Khẩu Cũ</td>
+                    <td><input type="password" name="password_cu"></td>
+                </tr>
+                <tr>
+                    <td>Mật Khẩu Mới</td>
+                    <td><input type="password" name="password_moi"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" name="doimatkhau" value="Đổi Mật Khẩu"></td>
+                </tr>
+            </table>
+        </form>
