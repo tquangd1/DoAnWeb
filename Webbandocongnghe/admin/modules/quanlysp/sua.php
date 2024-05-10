12e3a6f18c08144e19595f022a6c15434b77bde4
tquangd1 <162397376+tquangd1@users.noreply.github.com>
2024-05-10 18:11:07 +0700

    Update sua.php

--- Webbandocongnghe/admin/modules/quanlysp/sua.php
+++ Webbandocongnghe/admin/modules/quanlysp/sua.php
@@ -1,13 +1,13 @@
 <?php 
-    $sql_sua_danhmucsp = "SELECT * FROM tbl_sanpham WHERE id_danhmuc= '$_GET[idsanpham]' LIMIT 1";
+    $sql_sua_sp = "SELECT * FROM tbl_sanpham WHERE id_sanpham= '$_GET[idsanpham]' LIMIT 1";
     $query_sua_sp = mysqli_query($mysqli, $sql_sua_sp);
 ?>
-<p>Thêm sản phẩm</p>
+<p>Sửa sản phẩm</p>
 <table border="1" width="50%" style="border-collapse:collapse;">  
 <?php
 while($row = mysqli_fetch_array($query_sua_sp)){
 ?> 
-    <form method="POST" action="modules/quanlysp/xuly.php" enctype="multipart/form-data">
+    <form method="POST" action="modules/quanlysp/xuly.php?idsanpham=<?php echo $row['id_sanpham']; ?>" enctype="multipart/form-data">
         <tr>
             <td>Tên Sản Phẩm</td>
             <td><input type="text" value="<?php echo $row['tensanpham'] ?>" name="tensanpham"></td>
@@ -32,18 +32,40 @@ while($row = mysqli_fetch_array($query_sua_sp)){
         </tr>
         <tr>
             <td>Tóm tắt</td>
-            <td><textarea rows="5" name="tomtat" style="resize :none"><?php $row['tomtat'] ?></textarea></td>
+            <td><textarea rows="5" name="tomtat" style="resize :none"><?php echo $row['tomtat'] ?></textarea></td>
         </tr>
         <tr>
             <td>Nội Dung</td>
-            <td><textarea rows="5" name="noidung" style="resize :none"><?php $row['noidung'] ?></textarea></td>
+            <td><textarea rows="5" name="noidung" style="resize :none"><?php echo $row['noidung'] ?></textarea></td>
+        </tr>
+        <tr>
+            <td>Danh mục sản phẩm</td>
+            <td>
+                <select name="danhmuc">
+                    <?php
+                        $sql_danhmuc = "SELECT * FROM tbl_danhmuc ORDER BY id_danhmuc DESC";
+                        $query_danhmuc = mysqli_query($mysqli,$sql_danhmuc);
+                        while($row_danhmuc = mysqli_fetch_array( $query_danhmuc)){
+                            if($row_danhmuc['id_danhmuc']==$row['id_danhmuc']){
+                    ?>
+                    <option selected value="<?php echo $row_danhmuc['id_danhmuc']?>"><?php echo $row_danhmuc['tendanhmuc']?></option>
+                    <?php
+                        }else{
+                    ?>
+                    <option value="<?php echo $row_danhmuc['id_danhmuc']?>"><?php echo $row_danhmuc['tendanhmuc']?></option>
+                    <?php
+                        }
+                    }
+                    ?>
+                </select>
+            </td>
         </tr>
         <tr>
             <td>Tình Trạng</td>
             <td>
                 <select name="tinhtrang">
                     <?php
-                    if($row['tinhtrang'==1]){           
+                    if($row['ti nhtrang'==1]){           
                     ?>
                     <option value="1">Kích hoạt</option>
                     <option value="0">Ẩn</option>
@@ -61,7 +83,7 @@ while($row = mysqli_fetch_array($query_sua_sp)){
         </tr>
 
         <tr>
-            <td colspan="2"><input type="submit" name="suasanpham" value="Sửa sản phẩm"></td>
+            <td colspan="2"><input type="submit" name="suasanpham" value="Sửa   sản phẩm"></td>
         </tr>
     </form>
 <?php
