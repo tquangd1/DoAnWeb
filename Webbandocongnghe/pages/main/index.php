f81ad579ae090c4e6337df0ef0087eac6c749c8e
tquangd1 <162397376+tquangd1@users.noreply.github.com>
2024-05-10 18:14:00 +0700

    Update index.php

--- Webbandocongnghe/pages/main/index.php
+++ Webbandocongnghe/pages/main/index.php
@@ -1,73 +1,23 @@
+<?php
+$sql_pro = "SELECT * FROM tbl_sanpham, tbl_danhmuc WHERE tbl_sanpham.id_danhmuc = tbl_danhmuc.id_danhmuc ORDER BY
+ tbl_sanpham.id_sanpham DESC LIMIT 25";
+$query_pro = mysqli_query($mysqli, $sql_pro);
+?>
+
 <h3>Sản phẩm mới nhất</h3>
-                <ul class="product_list">
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>                
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                            </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="images/tainghe.webp">
-                            <p class="title_product">+ Tên sản phẩm: Tai nghe Sony.</p>
-                            <p class="price_product">+ Giá: 3.000.000đ.</p>
-                        </a>
-                    </li>
-                </ul>
+<ul class="product_list">
+    <?php
+    while ($row = mysqli_fetch_array($query_pro)) {
+    ?>
+        <li>
+            <a href="index.php?quanly=sanpham&id=<?php echo $row['id_sanpham'] ?>">
+                <img src="admin/modules/quanlysp/uploads/<?php echo $row['hinhanh'] ?>">
+                <p class="title_product">+ Tên sản phẩm: <?php echo $row['tensanpham'] ?> </p>
+                <p class="price_product">Giá sản phẩm: <?php echo number_format($row['giasp'],0,',','.').'vnd'?></p>
+                <p style="text-align: center;color:#d1d1d1"><?php echo $row['tendanhmuc'] ?></p>
+            </a>
+        </li>
+    <?php
+    }
+    ?>
+</ul>
